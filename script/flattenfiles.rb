ee81c0a5bc6c675d9922efbf5224b839d777a60a
Gerald Bauer <gerald.bauer@gmail.com>
2025-03-22 20:06:33 +0100

    up

---
+++ script/flattenfiles.rb
@@ -0,0 +1,84 @@
+############
+# to run use:
+#    $ ruby script/flattenfiles.rb
+
+require 'cocos'
+require 'fifa'
+
+
+##
+## !!! exclude france, netherlands & portugal for now!!!
+
+
+rootdir = '/sports/openfootball/europe'
+
+
+paths = Dir.glob( "#{rootdir}/**/*.txt" )
+
+## filter with season in dir
+
+SEASON_DIR = %r{ /
+                    \d{4}
+                  (-\d{2})?
+                 /}x
+     
+                 
+
+paths = paths.select { |path| SEASON_DIR.match( path ) }
+
+
+
+pp paths
+puts "   #{paths.size} candiate(s)"
+
+puts "---"
+
+paths.each do |path|
+  relpath = path[rootdir.size+1..-1] 
+
+  ## 
+  country, season, base = relpath.split( '/' )
+
+  puts "==> #{relpath}"
+  print "  "
+  pp country, season, base
+
+  ## get country code for country
+  cty = Fifa.world.find_by_name( country )
+  if cty.nil?
+    puts "!! ERROR - no country found for #{country}"
+    exit 1
+  end
+
+=begin
+  next if ['france',
+           'portugal',
+           'netherlands'].include?( country )
+=end
+
+  puts "#{cty.key} => #{country}"
+
+  name = "#{season}_#{cty.key}"
+  if base.start_with?('1')
+     name += '1'
+  elsif base.start_with?( '2')
+     name += '2'
+  elsif base.start_with?( 'cup')
+     name += 'cup'
+  else
+     raise ArgumentError, "unexpected basename #{base}"
+  end
+
+  name += ".txt"
+  puts "  >> #{name} <<"
+
+  oldpath = path
+  newpath = "#{rootdir}/#{country}/#{name}"
+
+  puts "  #{path}\n  #{newpath}"
+
+  FileUtils.mv( oldpath, newpath )
+
+end
+
+puts "bye"
